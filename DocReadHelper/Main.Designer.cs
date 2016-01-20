1ced0a1207a1cc9682efc532d97cc2ecdaec47bf
Mikhaltchenkov <mikhaltchenkov@gmail.com>
2016-01-20 19:47:14 +0300

    20/01/16 + Открытие документа; + Отслеживание факта закрытия документа и процесса Word.

---
+++ DocReadHelper/Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DocReadHelper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textDocumentFile = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnOpenDoc = new System.Windows.Forms.Button();
+            this.timerWatchDoc = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя файла:";
+            // 
+            // textDocumentFile
+            // 
+            this.textDocumentFile.Location = new System.Drawing.Point(85, 12);
+            this.textDocumentFile.Name = "textDocumentFile";
+            this.textDocumentFile.ReadOnly = true;
+            this.textDocumentFile.Size = new System.Drawing.Size(240, 20);
+            this.textDocumentFile.TabIndex = 1;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(331, 10);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(26, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnOpenDoc
+            // 
+            this.btnOpenDoc.Location = new System.Drawing.Point(363, 10);
+            this.btnOpenDoc.Name = "btnOpenDoc";
+            this.btnOpenDoc.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenDoc.TabIndex = 3;
+            this.btnOpenDoc.Text = "Открыть";
+            this.btnOpenDoc.UseVisualStyleBackColor = true;
+            this.btnOpenDoc.Click += new System.EventHandler(this.btnOpenDoc_Click);
+            // 
+            // timerWatchDoc
+            // 
+            this.timerWatchDoc.Tick += new System.EventHandler(this.timerWatchDoc_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 262);
+            this.Controls.Add(this.btnOpenDoc);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.textDocumentFile);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Document Read Helper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textDocumentFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnOpenDoc;
+        private System.Windows.Forms.Timer timerWatchDoc;
+    }
+}
+
---
+++ DocReadHelper/Main.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace DocReadHelper
+{
+    public partial class frmMain : Form
+    {
+        Word.Application oWord = null;
+        Word.Document oDoc = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Документы Microsoft Word|*.doc;*.docx;*.rtf";
+            ofd.RestoreDirectory = true ;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textDocumentFile.Text = ofd.FileName;
+            }
+            ofd.Dispose();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timerWatchDoc.Enabled = false;
+            if (oDoc != null)
+            {
+                try
+                {
+                    ((Word._Document)oDoc).Close();
+                }
+                catch { }
+            }/*
+            else
+            {
+                MessageBox.Show("Document is closed");
+            }*/
+            if (oWord != null)
+            {
+                try
+                {
+                    ((Word._Application)oWord).Quit();
+                }
+                catch { }
+            }
+        }
+
+        private void btnOpenDoc_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textDocumentFile.Text))
+            {
+                try
+                {
+                    if (oWord == null)
+                    {
+                        oWord = new Word.Application();
+                        oWord.Visible = false;
+                        ((Word.ApplicationEvents4_Event)oWord).Quit += delegate
+                        {
+                            oWord = null;
+                        };
+                    }
+                    if (oDoc != null)
+                    {
+                        ((Word._Document)oDoc).Close();
+                        //oDoc.Close();
+                    }
+                    oDoc = oWord.Documents.Open(textDocumentFile.Text);
+
+                    //Эвенты о закрытии документа
+                    ((Word.DocumentEvents_Event)oDoc).Close += delegate
+                    {
+                        oDoc = null;
+                    };
+
+                    oWord.Visible = true;
+                    btnOpenDoc.Enabled = false;
+                    btnOpenFile.Enabled = false;
+                }
+                catch(Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+                timerWatchDoc.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Выберите документ!");
+            }
+        }
+
+        private void DocumentClose()
+        {
+            oDoc = null;
+        }
+
+        private void timerWatchDoc_Tick(object sender, EventArgs e)
+        {
+            if (oDoc == null)
+            {
+                btnOpenFile.Enabled = true;
+                btnOpenDoc.Enabled = true;
+            }
+            if (oWord == null)
+            {
+                oDoc = null;
+                btnOpenFile.Enabled = true;
+                btnOpenDoc.Enabled = true;
+            }
+        }
+
+    }
+}
