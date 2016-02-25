c5d6ca6a9f0d6a8c2eaaeca79ced6b2ab76022e4
Dmitry Mikhaltchenkov <mikhaltchenkov@gmail.com>
2016-02-25 23:58:11 +0300

    25/02/16 + Реализация алгоритма помощника чтения; + Регулировка скорости чтения; + Изменения в интерфейсе;

--- DocReadHelper/Main.Designer.cs
+++ DocReadHelper/Main.Designer.cs
@@ -31,9 +31,18 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.textDocumentFile = new System.Windows.Forms.TextBox();
-            this.btnOpenFile = new System.Windows.Forms.Button();
-            this.btnOpenDoc = new System.Windows.Forms.Button();
             this.timerWatchDoc = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSpeedDown = new System.Windows.Forms.Button();
+            this.btnSpeedUp = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnToHome = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.textSpeed)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -51,46 +60,153 @@
             this.textDocumentFile.Name = "textDocumentFile";
             this.textDocumentFile.ReadOnly = true;
             this.textDocumentFile.Size = new System.Drawing.Size(240, 20);
-            this.textDocumentFile.TabIndex = 1;
-            // 
-            // btnOpenFile
-            // 
-            this.btnOpenFile.Location = new System.Drawing.Point(331, 10);
-            this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(26, 23);
-            this.btnOpenFile.TabIndex = 2;
-            this.btnOpenFile.Text = "...";
-            this.btnOpenFile.UseVisualStyleBackColor = true;
-            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
-            // 
-            // btnOpenDoc
-            // 
-            this.btnOpenDoc.Location = new System.Drawing.Point(363, 10);
-            this.btnOpenDoc.Name = "btnOpenDoc";
-            this.btnOpenDoc.Size = new System.Drawing.Size(75, 23);
-            this.btnOpenDoc.TabIndex = 3;
-            this.btnOpenDoc.Text = "Открыть";
-            this.btnOpenDoc.UseVisualStyleBackColor = true;
-            this.btnOpenDoc.Click += new System.EventHandler(this.btnOpenDoc_Click);
+            this.textDocumentFile.TabIndex = 0;
             // 
             // timerWatchDoc
             // 
             this.timerWatchDoc.Tick += new System.EventHandler(this.timerWatchDoc_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Скорость чтения:";
+            // 
+            // textSpeed
+            // 
+            this.textSpeed.Location = new System.Drawing.Point(113, 42);
+            this.textSpeed.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.textSpeed.Minimum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.textSpeed.Name = "textSpeed";
+            this.textSpeed.Size = new System.Drawing.Size(60, 20);
+            this.textSpeed.TabIndex = 3;
+            this.textSpeed.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.textSpeed.ValueChanged += new System.EventHandler(this.textSpeed_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "слов/мин.";
+            // 
+            // btnSpeedDown
+            // 
+            this.btnSpeedDown.Enabled = false;
+            this.btnSpeedDown.Image = global::DocReadHelper.Properties.Resources.control_rewind_blue;
+            this.btnSpeedDown.Location = new System.Drawing.Point(276, 39);
+            this.btnSpeedDown.Name = "btnSpeedDown";
+            this.btnSpeedDown.Size = new System.Drawing.Size(23, 23);
+            this.btnSpeedDown.TabIndex = 5;
+            this.btnSpeedDown.UseVisualStyleBackColor = true;
+            this.btnSpeedDown.Click += new System.EventHandler(this.btnSpeedDown_Click);
+            // 
+            // btnSpeedUp
+            // 
+            this.btnSpeedUp.Enabled = false;
+            this.btnSpeedUp.Image = global::DocReadHelper.Properties.Resources.control_fastforward_blue;
+            this.btnSpeedUp.Location = new System.Drawing.Point(363, 39);
+            this.btnSpeedUp.Name = "btnSpeedUp";
+            this.btnSpeedUp.Size = new System.Drawing.Size(23, 23);
+            this.btnSpeedUp.TabIndex = 8;
+            this.btnSpeedUp.UseVisualStyleBackColor = true;
+            this.btnSpeedUp.Click += new System.EventHandler(this.btnSpeedUp_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Image = global::DocReadHelper.Properties.Resources.control_pause_blue;
+            this.btnStop.Location = new System.Drawing.Point(334, 39);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Image = global::DocReadHelper.Properties.Resources.control_play_blue;
+            this.btnStart.Location = new System.Drawing.Point(305, 39);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(23, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnToHome
+            // 
+            this.btnToHome.Enabled = false;
+            this.btnToHome.Image = global::DocReadHelper.Properties.Resources.control_start_blue;
+            this.btnToHome.Location = new System.Drawing.Point(247, 39);
+            this.btnToHome.Name = "btnToHome";
+            this.btnToHome.Size = new System.Drawing.Size(23, 23);
+            this.btnToHome.TabIndex = 4;
+            this.btnToHome.UseVisualStyleBackColor = true;
+            this.btnToHome.Click += new System.EventHandler(this.btnToHome_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Image = global::DocReadHelper.Properties.Resources._49;
+            this.btnHelp.Location = new System.Drawing.Point(363, 10);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(23, 23);
+            this.btnHelp.TabIndex = 2;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Image = global::DocReadHelper.Properties.Resources.folder_page;
+            this.btnOpenFile.Location = new System.Drawing.Point(331, 10);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(26, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(652, 262);
-            this.Controls.Add(this.btnOpenDoc);
+            this.ClientSize = new System.Drawing.Size(402, 68);
+            this.Controls.Add(this.btnSpeedDown);
+            this.Controls.Add(this.btnSpeedUp);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnToHome);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textSpeed);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnOpenFile);
             this.Controls.Add(this.textDocumentFile);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "Document Read Helper";
+            this.TopMost = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.textSpeed)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,8 +217,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textDocumentFile;
         private System.Windows.Forms.Button btnOpenFile;
-        private System.Windows.Forms.Button btnOpenDoc;
         private System.Windows.Forms.Timer timerWatchDoc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown textSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnToHome;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSpeedUp;
+        private System.Windows.Forms.Button btnSpeedDown;
     }
 }
 
--- DocReadHelper/Main.cs
+++ DocReadHelper/Main.cs
@@ -5,14 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
 using Word = Microsoft.Office.Interop.Word;
 
 namespace DocReadHelper
 {
     public partial class frmMain : Form
     {
-        Word.Application oWord = null;
-        Word.Document oDoc = null;
+        static Word.Application oWord = null;
+        static Word.Document oDoc = null;
+        static int wLenght = 1;
+        static int wSpeed = 100;
+        System.Windows.Forms.Timer timer = null;
 
         public frmMain()
         {
@@ -27,6 +31,49 @@ namespace DocReadHelper
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 textDocumentFile.Text = ofd.FileName;
+                if (!string.IsNullOrEmpty(textDocumentFile.Text))
+                {
+                    try
+                    {
+                        if (oWord == null)
+                        {
+                            oWord = new Word.Application();
+                            oWord.Visible = false;
+                            ((Word.ApplicationEvents4_Event)oWord).Quit += delegate
+                            {
+                                oWord = null;
+                            };
+                        }
+                        if (oDoc != null)
+                        {
+                            ((Word._Document)oDoc).Close();
+                            //oDoc.Close();
+                        }
+                        oDoc = oWord.Documents.Open(textDocumentFile.Text);
+
+                        //Эвенты о закрытии документа
+                        ((Word.DocumentEvents_Event)oDoc).Close += delegate
+                        {
+                            oDoc = null;
+                        };
+
+                        oWord.Visible = true;
+                        btnStart.Enabled = true;
+                        btnToHome.Enabled = true;                        
+
+//                        btnOpenDoc.Enabled = false;
+                        btnOpenFile.Enabled = false;
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show(exc.Message);
+                    }
+                    timerWatchDoc.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Выберите документ!");
+                }
             }
             ofd.Dispose();
         }
@@ -34,6 +81,8 @@ namespace DocReadHelper
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             timerWatchDoc.Enabled = false;
+            if (timer != null) btnStop_Click(sender, e);
+            /*
             if (oDoc != null)
             {
                 try
@@ -41,68 +90,31 @@ namespace DocReadHelper
                     ((Word._Document)oDoc).Close();
                 }
                 catch { }
-            }/*
+            }*/
+             /*
             else
             {
                 MessageBox.Show("Document is closed");
             }*/
-            if (oWord != null)
+            /*if (oWord != null)
             {
                 try
                 {
                     ((Word._Application)oWord).Quit();
                 }
                 catch { }
-            }
+            }*/
         }
 
         private void btnOpenDoc_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(textDocumentFile.Text))
-            {
-                try
-                {
-                    if (oWord == null)
-                    {
-                        oWord = new Word.Application();
-                        oWord.Visible = false;
-                        ((Word.ApplicationEvents4_Event)oWord).Quit += delegate
-                        {
-                            oWord = null;
-                        };
-                    }
-                    if (oDoc != null)
-                    {
-                        ((Word._Document)oDoc).Close();
-                        //oDoc.Close();
-                    }
-                    oDoc = oWord.Documents.Open(textDocumentFile.Text);
-
-                    //Эвенты о закрытии документа
-                    ((Word.DocumentEvents_Event)oDoc).Close += delegate
-                    {
-                        oDoc = null;
-                    };
-
-                    oWord.Visible = true;
-                    btnOpenDoc.Enabled = false;
-                    btnOpenFile.Enabled = false;
-                }
-                catch(Exception exc)
-                {
-                    MessageBox.Show(exc.Message);
-                }
-                timerWatchDoc.Enabled = true;
-            }
-            else
-            {
-                MessageBox.Show("Выберите документ!");
-            }
+            
         }
 
         private void DocumentClose()
         {
             oDoc = null;
+
         }
 
         private void timerWatchDoc_Tick(object sender, EventArgs e)
@@ -110,14 +122,164 @@ namespace DocReadHelper
             if (oDoc == null)
             {
                 btnOpenFile.Enabled = true;
-                btnOpenDoc.Enabled = true;
+//                btnOpenDoc.Enabled = true;
             }
             if (oWord == null)
             {
                 oDoc = null;
                 btnOpenFile.Enabled = true;
-                btnOpenDoc.Enabled = true;
+//                btnOpenDoc.Enabled = true;
             }
+            if (oDoc == null || oWord == null)
+            {
+                btnSpeedDown.Enabled = false;
+                btnSpeedUp.Enabled = false;
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+                btnToHome.Enabled = false;
+            }
+        }
+
+        private void btnToHome_Click(object sender, EventArgs e)
+        {
+            if (oDoc != null)
+            {
+                Word.Range rng = oDoc.Range(0, 0);
+                rng.Select();
+                //oWord.Selection.MoveStart();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            wSpeed = (int)textSpeed.Value;
+            if (oWord != null)
+            {
+                oWord.ShowMe();
+            }
+
+            if (timer == null)
+            {
+                timer = new Timer();
+                timer.Interval = 50;
+                timer.Tick += NextWord;
+                timer.Start();
+                //timer = new System.Timers.Timer(100);
+                //timer.Elapsed += NextWord;
+            }
+            btnSpeedDown.Enabled = true;
+            btnSpeedUp.Enabled = true;
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            this.Opacity = 50;
+            //NextWord();
+        }
+
+        private static void NextWord(Object myObject, EventArgs e)
+        {
+            if (oDoc != null)
+            {
+                if (--wLenght == 0)
+                {
+                    oWord.Selection.MoveRight(Word.WdUnits.wdWord, 1);
+                    oWord.Selection.Expand(Word.WdUnits.wdWord);
+                    wLenght = (1200 / (wSpeed * 5)) * oWord.Selection.Characters.Count;
+                    if (wLenght == 0) wLenght = 1;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnSpeedUp.Enabled = false;
+            btnSpeedDown.Enabled = false;
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Dispose();
+                timer = null;
+            }
+            this.Opacity = 100;
+        }
+
+        private void btnSpeedUp_Click(object sender, EventArgs e)
+        {
+            if (textSpeed.Value < textSpeed.Maximum)
+            {
+                wSpeed = (int)((textSpeed.Value + 10)>textSpeed.Maximum?textSpeed.Maximum:textSpeed.Value + 10);
+                textSpeed.Value = wSpeed;
+            }
+            if (oWord != null)
+            {
+                oWord.ShowMe();
+            }
+        }
+
+        private void btnSpeedDown_Click(object sender, EventArgs e)
+        {
+            if (textSpeed.Value > textSpeed.Minimum)
+            {
+                wSpeed = (int)((textSpeed.Value - 10) < textSpeed.Minimum ? textSpeed.Minimum : textSpeed.Value - 10);
+                textSpeed.Value = wSpeed;
+            }
+            if (oWord != null)
+            {
+                oWord.ShowMe();
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            /*
+             * To set the version number, create/edit AssemblyInfo.cs
+
+             * [assembly: AssemblyVersion("1.0.*")]
+             * [assembly: AssemblyFileVersion("1.0.*")]
+             * Also as a side note, the third number is the number of days since 2/1/2000
+             * and the fourth number is half of the amount of total seconds in the day.
+             * So if you compile at midnight it should be zero.
+             */
+            //string version = AssemblyVersion;
+            DateTime date = new DateTime(2000, 1, 1);
+            bool check = false;
+            string[] verinfo = AssemblyVersion.Split('.');
+            try
+            {
+                //int days = Convert.ToInt32(verinfo[2]);
+                date = date.AddDays(Convert.ToInt32(verinfo[2]));
+                date = date.AddSeconds(Convert.ToInt32(verinfo[3]) * 2);
+                check = true;
+            }
+            catch { }
+            if (check)
+            {
+                MessageBox.Show(string.Format("Document Reading Helper ver.{0}.{1}\r\nВремя сборки: {2}\r\n\r\nРазработчик:\r\n\tМихальченков Дмитрий\r\n\tmikhaltchenkov@gmail.com", verinfo[0], verinfo[1], date), "О программе...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Document Reading Helper ver.{0}\r\n\r\nРазработчик:\r\n\tМихальченков Дмитрий\r\n\tmikhaltchenkov@gmail.com", AssemblyVersion), "О программе...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        private void textSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            wSpeed = (int)textSpeed.Value;
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            string[] verinfo = AssemblyVersion.Split('.');
+            this.Text = string.Format("Document Reading Helper v.{0}.{1}",verinfo[0],verinfo[1]);
         }
 
     }
